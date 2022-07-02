8569993bb7180250de9e443cb15aabf7dbcfcac3
Brian Keating <brikeats@gmail.com>
2022-07-02 14:40:44 -0700

    flask app boilerplate

--- README.md
+++ README.md
@@ -8,4 +8,14 @@ An app for AR display of sun's trajectory.
 
 `pip install requirements.txt`
 
+`export FLASK_APP=app`
+
+`export FLASK_ENV=development  # only for local testing; NOT for real deployment`
+
+`flask run --host=0.0.0.0`
+
+[View in browser](http://127.0.0.1:5000/)
+
+------
+
 `python sunpos.py  # prints sun coordinates at sunset`
---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, send_from_directory, render_template
+
+
+app = Flask(
+    __name__,
+    static_folder='static',
+    template_folder='templates'
+)
+
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
